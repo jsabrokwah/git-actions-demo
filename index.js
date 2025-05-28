530cb72b99b6bcfcd7a9906388dea8756cff5354
JS-Abrokwah <jsabrokwahsenior@gmail.com>
2025-05-28 16:21:24 +0000

    Built a simple nodejs app

---
+++ index.js
@@ -0,0 +1,14 @@
+// Simple Hello World Node.js application
+const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
